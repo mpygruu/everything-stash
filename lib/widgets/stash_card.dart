0b37d4756924799c4c17bca2f398888e393ddecf
mpygru <barkwa67@gmail.com>
2022-03-29 22:45:47 +0200

    Improved stash card widget's constructor

--- lib/widgets/stash_card.dart
+++ lib/widgets/stash_card.dart
@@ -1,3 +1,4 @@
+import 'package:everything_stash/pages/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:everything_stash/models/db_model.dart';
 import 'package:focused_menu/focused_menu.dart';
@@ -6,33 +7,34 @@ import 'package:focused_menu/modals.dart';
 import '../models/stash.dart';
 
 class StashCard extends StatelessWidget {
-  final String? title;
-  final String? description;
+  final Stash? stash;
 
-  const StashCard({this.title, this.description, Key? key}) : super(key: key);
+  const StashCard(this.stash, {Key? key}) : super(key: key);
 
   void openStash() {}
-
-  void deleteStash(Stash stash) {
-    var db = DatabaseConnector();
-    db.deleteStash(stash);
-  }
 
   @override
   Widget build(BuildContext context) {
     return FocusedMenuHolder(
-      onPressed: () {
-        //implement deleting a stash
-      },
+      onPressed: () {},
       menuItems: <FocusedMenuItem>[
         FocusedMenuItem(title: const Text('Open'), onPressed: () {}),
-        FocusedMenuItem(title: const Text('Delete'), onPressed: () {})
+        FocusedMenuItem(
+            title: const Text('Delete'),
+            onPressed: () {
+              var db = DatabaseConnector();
+              db.deleteStash(stash);
+              Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => const MainPage()));
+            })
       ],
       child: Card(
         child: ListTile(
           leading: const Icon(Icons.storage_rounded, size: 38),
-          title: Text(title.toString()),
-          subtitle: Text(description.toString()),
+          title: Text(stash!.title.toString()),
+          subtitle: Text(stash!.description.toString()),
           trailing: IconButton(
             icon: const Icon(Icons.more_vert),
             onPressed: () {},
