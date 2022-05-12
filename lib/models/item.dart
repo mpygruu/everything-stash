b1d8db54cd68f8e5c436c9f51e37d2722c6d73cd
mpygru <barkwa67@gmail.com>
2022-05-12 11:41:35 +0200

    Added item mapping

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -1,8 +1,22 @@
-import 'package:image/image.dart';
-
 class Item {
   int? id;
   String? name;
   String? description;
-  Image? image;
+  //Image? image;
+
+  Item({this.id, this.name, this.description});
+
+  factory Item.fromMap(Map<String, dynamic> json) => Item(
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+      );
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+    };
+  }
 }
