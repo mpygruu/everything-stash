1b92f0565ab9fa89d12518e70cf3f0de072975fa
mpygru <barkwa67@gmail.com>
2022-05-12 11:41:52 +0200

    Added popupmenu for stashcard

--- lib/widgets/stash_card.dart
+++ lib/widgets/stash_card.dart
@@ -1,4 +1,6 @@
+import 'package:everything_stash/models/db_model.dart';
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 
 class StashCard extends StatelessWidget {
   final String? title;
@@ -15,13 +17,21 @@ class StashCard extends StatelessWidget {
         leading: const Icon(Icons.storage_rounded, size: 38),
         title: Text(title.toString()),
         subtitle: Text(description.toString()),
-        trailing: IconButton(
+        trailing: PopupMenuButton(
+          onSelected: (result) {
+            if (result == 0) {
+              //DatabaseConnector.database;
+            }
+          },
           icon: const Icon(Icons.more_vert),
-          onPressed: () {},
+          itemBuilder: (BuildContext context) => [
+            const PopupMenuItem(
+              child: Text("Delete"),
+              value: 0,
+            ),
+          ],
         ),
         onTap: () {},
-        //onLongPress will execute the same function as trailing
-        onLongPress: () {},
       ),
     );
   }
